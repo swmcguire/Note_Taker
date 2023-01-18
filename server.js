3e55b037a22d7070c468dcac6760e4434854e523
swmcguire <swmcguire1@comcast.net>
2023-01-17 20:12:22 -0600

    Added Read Me and updated servers.js

--- server.js
+++ server.js
@@ -8,15 +8,22 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}))
 
+//------------------------ALLOWS ACCESS TO THE PUBLIC PATH------------------------//
 app.use(express.static('public'));
 
+
+//------------------------GET ROUTE FOR HOMEPAGE------------------------//
 app.get('/', (req, res) =>
     res.sendFile(path.join(_dirname, '/public/index.html'))
 );
 
+
+//------------------------GET ROUTE FOR NOTES PAGE------------------------//
 app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+
+//------------------------LISTEN METHOD BINDS WITH HOST/ PORT------------------------//
 app.listen(PORT, () => console.log(`App listening on port http://localhost:${PORT}`));
 
