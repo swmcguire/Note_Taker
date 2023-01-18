06dad1b59e9ac7563941e7b1c3162a0bfd74b7d8
swmcguire <swmcguire1@comcast.net>
2023-01-17 21:21:02 -0600

    Added routes, created notes.js, fsUtils.js, and index.js

---
+++ routes/notes.js
@@ -0,0 +1,35 @@
+const notes = require('express').Router();
+//const { v4: uuidv4 } = require('uuid');
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+} = require('../helpers/fsUtils');
+
+
+//------------------ GET ROUTE FOR RETREIVING ALL THE NOTES ------------------//
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+
+//------------------ POST ROUTE FOR A NEW NOTE ------------------//
+notes.post('/', (req, res) => {
+    console.log(req.body);
+
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title, 
+            text,
+        };
+
+        readAndAppend(newNote, '.db/db.json');
+        res.json(`Note added succcessfully`);
+    } else {
+        res.errored(`Error in adding tip`);
+    }
+});
+
+module.exports = notes;
